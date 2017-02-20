262dd4d87d1d4dee05b31db74d5e45f9d802c206
victoriasloan <victoriasloan95@gmail.com>
2017-02-20 13:12:47 +0000

    basic todo implemented before changing to movies

--- src/index.js
+++ src/index.js
@@ -1,1 +1,26 @@
-console.log('hi');
+import React from 'react';
+import { createStore } from 'redux';
+
+const movies = (state = 0, action) => {
+        switch (action.type) {
+        case 'INCREMENT':
+         return state + 1;
+         case 'DECREMENT':
+         return state -1;
+         default:
+         return state;
+    }
+};
+
+const store = createStore(movies);
+
+const render = () => {
+    document.body.innerText = store.getState();
+};
+
+store.subscribe(render);
+render();
+
+document.addEventListener('click', () => {
+    store.dispatch({ type: "DECREMENT"});
+});
